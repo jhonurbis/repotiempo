4789e0c3b4e7a50c571610dc91b2d7fd5df2fa39
jhonurbis <44198349+jhonurbis@users.noreply.github.com>
2018-10-19 12:07:52 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+// Dependencies 
+const express = require('express');
+const bodyparser = require('body-parser');
+
+// Configuration
+const app = express();
+app.use(bodyparser.json());
+
+// Webhook route
+app.post('/webhook', (req, res) => {
+	const data = req.body;
+
+	// Code the task you want to achieve with @data
+	// Read the v2 api documentation of dialogflow : https://dialogflow.com/docs/fulfillment
+	// Using the v2 will become mandatory, Google wrote a guide to migrate from v1 to v2 as v2 is officially released
+
+	const response = {
+		fulfillmentText: "Your webhook works fine !",
+	}
+	res.json(response);
+});
